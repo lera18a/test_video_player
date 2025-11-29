e23e44a02fce0c7c5fc77ada19bf402e1d460c22
Badrakova Valeria <lera_18a@mail.ru>
2025-11-29 13:36:57 +0300

    add api

---
+++ lib/api/model/model_video.dart
@@ -0,0 +1,11 @@
+class ModelVideo {
+  final String title;
+  final String description;
+  final String url;
+
+  ModelVideo({
+    required this.title,
+    required this.description,
+    required this.url,
+  });
+}
