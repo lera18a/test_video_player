6f0d62ed1db50483eabe4f3f0278ed9053bf9111
Badrakova Valeria <lera_18a@mail.ru>
2025-12-03 18:43:41 +0300

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_video_player/api/mock_video_api.dart';
+import 'package:test_video_player/bloc/data_bloc/data_bloc.dart';
 import 'package:test_video_player/bloc/settings_bloc/settings_bloc.dart';
 import 'package:test_video_player/bloc/theme_bloc/theme_bloc.dart';
 import 'package:test_video_player/bloc/video_bloc/video_bloc.dart';
 import 'package:test_video_player/bloc/video_nav_bloc/video_nav_bloc.dart';
 import 'package:test_video_player/bloc/volume_bloc/volume_bloc.dart';
-import 'package:test_video_player/theme/theme.dart';
-import 'package:test_video_player/ui/pages/home_page.dart';
+import 'package:test_video_player/ui/logic_ui/app_orientation.dart';
+import 'package:test_video_player/ui/logic_ui/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,24 +19,22 @@ void main() async {
     DeviceOrientation.landscapeRight,
   ]);
   runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => VolumeBloc()),
-        BlocProvider(create: (context) => VideoNavBloc()),
-        BlocProvider(create: (context) => VideoBloc()),
-        BlocProvider(create: (context) => ThemeBloc()),
-        BlocProvider(create: (context) => SettingsBloc()),
-      ],
-      child: BlocBuilder<ThemeBloc, ThemeState>(
-        builder: (context, themState) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: lightTheme,
-            darkTheme: darkTheme,
-            themeMode: themState.isDark ? ThemeMode.light : ThemeMode.dark,
-            home: const HomePage(),
-          );
-        },
+    RepositoryProvider<MockVideoApi>(
+      create: (_) => MockVideoApi(),
+
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => SettingsBloc()),
+          BlocProvider(create: (context) => ThemeBloc()),
+
+          BlocProvider(
+            create: (context) => DataBloc(context.read<MockVideoApi>()),
+          ),
+          BlocProvider(create: (context) => VolumeBloc()),
+          BlocProvider(create: (context) => VideoNavBloc()),
+          BlocProvider(create: (context) => VideoBloc()),
+        ],
+        child: AppTheme(child: AppOrientation()),
       ),
     ),
   );
